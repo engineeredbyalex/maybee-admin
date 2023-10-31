7864596cb5fef9dd042b9f5daf9bdac32d03d325
engineeredbyalex <nonstopzen26@gmail.com>
2023-10-31 10:37:15 +0200

    31.10 rev-8

--- pages/api/products.js
+++ pages/api/products.js
@@ -15,43 +15,24 @@ export default async function handle(req, res) {
     }
   }
 
-  else if (req.method === 'POST') {
-    console.log("POST Product:", req.body);
-    try {
-      const product = await Product.create(req.body);
-      console.log("Product Created:", product);
-      res.json(product);
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Failed to create the product" });
-    }
-  } else if (req.method === 'PUT') {
-    const { _id, scente, ...productData } = req.body; // Extract scente separately
-    console.log("PUT Product:", _id, productData, scente);
+  if (method === 'POST') {
+    const { title, description, price, images, category, properties } = req.body;
+    const productDoc = await Product.create({
+      title, description, price, images, category, properties,
+    })
+    res.json(productDoc);
+  }
 
-    try {
-      const updatedProduct = await Product.findOneAndUpdate(
-        { _id },
-        { ...productData, scente },
-        { new: true } // Return the updated product
-      );
-      console.log("Product Updated:", updatedProduct);
-      res.json(updatedProduct);
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Failed to update the product" });
-    }
-  } else if (req.method === 'DELETE') {
+  if (method === 'PUT') {
+    const { title, description, price, images, category, properties, _id } = req.body;
+    await Product.updateOne({ _id }, { title, description, price, images, category, properties });
+    res.json(true);
+  }
+
+  if (method === 'DELETE') {
     if (req.query?.id) {
-      console.log("DELETE Product by ID:", req.query.id);
-      try {
-        await Product.deleteOne({ _id: req.query.id });
-        console.log("Product Deleted");
-        res.json(true);
-      } catch (error) {
-        console.error("Error:", error);
-        res.status(500).json({ error: "Failed to delete the product" });
-      }
+      await Product.deleteOne({ _id: req.query?.id });
+      res.json(true);
     }
   }
 }
