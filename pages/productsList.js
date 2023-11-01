b356704eb2a5a30d8539b3c404f485ace71b959c
engineeredbyalex <nonstopzen26@gmail.com>
2023-11-01 23:50:02 +0200

    testing

--- pages/productsList.js
+++ pages/productsList.js
@@ -19,9 +19,9 @@ export default function ProductsList() {
             <div className='w-full h-full flex items-center justify-center'>
                 <div className='grid grid-cols-3 gap-10'>
                     {products.map(product => (
-                        <Link href={`/products/[id]`} as={`/products/${product._id}`}>
+                        <Link key={product._id} href={`/products/[id]`} as={`/products/${product._id}`}>
                             <div className='bg-[#ececec] py-10 flex items-center justify-center flex-col text-center rounded-xl' key={product._id}>
-                                <img key={images._id} className='w-[150px] h-[150px] rounded-xl' src={product.images[0]}></img>
+                                <img className='w-[150px] h-[150px] rounded-xl' src={product.images[0]}></img>
                                 <p className='mt-10'>{product.title}</p>
                             </div>
                         </Link>
